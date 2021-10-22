4ff5d140bcdbb7f250d5dc0fcbfacc453f2bed7a
nrc4867 <nrc4867@rit.edu>
2021-10-22 00:10:08 -0400

    Made services take console output

--- src/main/kotlin/dev/chieppa/controller/ServicesController.kt
+++ src/main/kotlin/dev/chieppa/controller/ServicesController.kt
@@ -2,10 +2,40 @@ package dev.chieppa.controller
 
 import dev.chieppa.model.response.ServiceComplete
 import dev.chieppa.util.createPath
+import mu.KotlinLogging
+import java.io.File
+
+val logger = KotlinLogging.logger { }
+
+val system32directory = listOf("C:", "Windows", "System32")
 
 fun startSSH(): ServiceComplete {
+    val service = startService(
+        listOf("wsl.exe", "--user", "root", "service", "ssh", "start"),
+        workingDirectory = system32directory
+    )
+
+    return ServiceComplete(consoleOutput = service.first, exitCode = service.second)
+}
+
+
+fun startService(
+    command: List<String>,
+    env: List<String> = listOf(),
+    workingDirectory: List<String>
+): Pair<String, Int> {
     val runtime = Runtime.getRuntime()
-    val process = runtime.exec(listOf("C:", "Windows", "System32", "wsl.exe").createPath() + " --user root service ssh start")
-    process.waitFor()
-    return ServiceComplete(exitCode = process.exitValue())
+    val process = runtime.exec(
+        command.toTypedArray(),
+        env.toTypedArray(),
+        File(workingDirectory.createPath())
+    )
+    val output = process.inputStream.use {
+        process.waitFor()
+        it.reader().use { reader -> reader.readText() }
+    }
+
+    logger.info { "Process exited with value: ${process.exitValue()}\n$output" }
+
+    return Pair(output, process.exitValue())
 }
